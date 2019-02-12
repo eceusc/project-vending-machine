daf49aa2a983bef2c0b3a01196791c4bae045c4b
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-02-12 14:09:34 -0800

    Add models/ directory, simple Owner model

---
+++ web_server/web_server/models/__init__.py
@@ -0,0 +1,1 @@
+from .Owner import Owner
--- web_server/web_server/settings.py
+++ web_server/web_server/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web_server',
 ]
 
 MIDDLEWARE = [
