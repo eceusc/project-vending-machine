77c8dbd8a0699f35cceda8fa00db023243e7130e
Chris <mrchrisrobles@gmail.com>
2019-02-12 17:53:51 -0800

    edited models

--- web_server/web_server/models/Contributions.py
+++ web_server/web_server/models/Contributions.py
@@ -1,8 +1,17 @@
 from django.db import models
+from enum import Enum
+
+class ContributorType(Enum):
+    AD = "Admin"
+    ME = "Member"
+
 
 class Contributions(models.Model):
     id = models.IntegerField()
-    type = models. # not too sure what to put here. maybe Field.choices?
+    type = models.CharField(
+        max_length=10,
+        choices=[(tag, tag.value) for tag in ContributorType]
+    )
     contributor = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     approval = models.IntegerField()
