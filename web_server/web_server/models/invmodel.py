7c373e1ab98a787e742fbf8b1b4f2eb0cd767acf
Masaya Honda <jp.masaya@gmail.com>
2019-02-12 17:56:29 -0800

    Added a new invmodel model

---
+++ web_server/web_server/models/invmodel.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class invmodel(models.Model);
+    id = models.IntField(primary_key=True)
+    machine = models.IntField
+    item = models.IntField
+    transaction = models.IntField
