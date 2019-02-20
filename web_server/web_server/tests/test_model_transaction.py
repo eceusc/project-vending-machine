0648b3c664369d31cd7406f4dd01a448803ffd0a
cpbenite <cpbenite@ucsd.edu>
2019-02-19 17:10:43 -0800

    Updated

--- web_server/web_server/tests/test_model_transaction.py
+++ web_server/web_server/tests/test_model_transaction.py
@@ -4,12 +4,11 @@ from web_server.models import transaction
 from django.utils import timezone
 
 class TransactionTest(TestCase):
-    x = timezone.now()
 
-    def create_transaction(self, user_id=12345678, user=1, timestamp=x):
-        return transaction.objects.create(user_id=user_id, user=user, timestamp=timestamp)
+    def create_transaction(self, user_id=12345678, user=1):
+        return transaction.objects.create(user_id=user_id, user=user)
 
-    def test_transaction_creation(self, x=x):
+    def test_transaction_creation(self):
         o1 = self.create_transaction()
         o2 = self.create_transaction(user_id=10000000, user=2)
 
@@ -18,6 +17,3 @@ class TransactionTest(TestCase):
 
         self.assertEqual(1, o1.user)
         self.assertEqual(2, o2.user)
-
-        self.assertEqual(x, o1.timestamp)
-        self.assertEqual(x, o2.timestamp)
