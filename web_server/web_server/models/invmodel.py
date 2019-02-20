1ba52ed09b0d97867e6f60e57a00ee27acbb9f5b
Masaya Honda <jp.masaya@gmail.com>
2019-02-19 16:35:27 -0800

    made changes to xyz

--- web_server/web_server/models/invmodel.py
+++ web_server/web_server/models/invmodel.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class invmodel(models.Model);
-    id = models.IntField(primary_key=True)
-    machine = models.IntField
-    item = models.IntField
-    transaction = models.IntField
+class invmodel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    machine = models.IntegerField()
+    item = models.IntegerField()
+    transaction = models.IntegerField()
