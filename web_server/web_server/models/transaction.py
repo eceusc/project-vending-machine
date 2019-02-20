7ca8db33977934eede36ba6c19ffd5f401a24af7
cpbenite <cpbenite@ucsd.edu>
2019-02-19 17:03:11 -0800

    Updated Date field

--- web_server/web_server/models/transaction.py
+++ web_server/web_server/models/transaction.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 class transaction(models.Model):
 	# do stuff
 	user_id = models.IntegerField()
 	user = models.IntegerField()
-	timestamp = models.DateField()
+	timestamp = models.DateField(default=timezone.now())
 	# test
--- web_server/web_server/tests/test_model_transaction.py
+++ web_server/web_server/tests/test_model_transaction.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from web_server.models import transaction
-import datetime
+#import datetime
+from django.utils import timezone
 
 class TransactionTest(TestCase):
+    x = timezone.now()
 
-    def create_transaction(self, user_id=12345678, user=1, timestamp=datetime.date.today):
+    def create_transaction(self, user_id=12345678, user=1, timestamp=x):
         return transaction.objects.create(user_id=user_id, user=user, timestamp=timestamp)
 
-    def test_transaction_creation(self):
+    def test_transaction_creation(self, x=x):
         o1 = self.create_transaction()
-        o2 = self.create_transaction(user_id=10000000, user=2, timestamp=datetime.date.today)
+        o2 = self.create_transaction(user_id=10000000, user=2)
 
         self.assertEqual(12345678, o1.user_id)
         self.assertEqual(10000000, o2.user_id)
@@ -17,5 +19,5 @@ class TransactionTest(TestCase):
         self.assertEqual(1, o1.user)
         self.assertEqual(2, o2.user)
 
-        self.assertEqual(datetime.date.today, o1.timestamp)
-        self.assertEqual(datetime.date.today, o2.timestamp)
+        self.assertEqual(x, o1.timestamp)
+        self.assertEqual(x, o2.timestamp)
